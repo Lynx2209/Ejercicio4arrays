cc89c7c835a7ff5b630d1e26aa0cbc3a6a93132d
Lynx2209 <csi23-jconazo@colegioaltair.net>
2023-12-18 14:20:51 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercicio4arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el tamaño del vector");
+            int longitud = Convert.ToInt32(Console.ReadLine());
+
+            int[] numerorandom = new int[longitud];
+
+            Random random = new Random();
+
+            for (int i = 0; i < longitud; i++)
+            {
+                int numeroguardar = random.Next(1, 1000);
+                numerorandom[i] = numeroguardar;
+
+                
+            }
+            Array.Sort(numerorandom);
+            for (int i = 0;i < longitud; i++)
+            {
+                Console.WriteLine(numerorandom[i]);
+            }
+        }
+    }
+
+}
